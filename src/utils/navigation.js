aac285ceb682aff2c56b7aaad16807af5730851b
Regis Reis <60001184+rhreis504@users.noreply.github.com>
2026-05-08 14:19:28 -0300

    Fix GitHub Pages routing base path

--- src/utils/navigation.js
+++ src/utils/navigation.js
@@ -1,4 +1,17 @@
-export function navigateTo(path) {
-  window.history.pushState({}, '', path);
-  window.dispatchEvent(new PopStateEvent('popstate'));
+import { toBrowserPath } from './basePath';
+
+function emitRouteChange() {
+  const event = typeof PopStateEvent === 'function' ? new PopStateEvent('popstate') : new Event('popstate');
+  window.dispatchEvent(event);
+}
+
+export function navigateTo(path, { replace = false } = {}) {
+  const browserPath = toBrowserPath(path);
+  const historyAction = replace ? 'replaceState' : 'pushState';
+
+  if (window.location.pathname !== browserPath || window.location.search || window.location.hash) {
+    window.history[historyAction]({}, '', browserPath);
+  }
+
+  emitRouteChange();
 }
